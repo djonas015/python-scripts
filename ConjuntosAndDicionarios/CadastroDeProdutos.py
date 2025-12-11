eebe7446a6250018fcfe01620b544a9670b97546
Jonas Fernandes <diasjonas121@gmail.com>
2025-12-11 20:43:00 -0300

    v1

---
+++ ConjuntosAndDicionarios/CadastroDeProdutos.py
@@ -0,0 +1,6 @@
+dic1 = {}
+for i in range(3):
+    produto1 = input("Digite o nome do produto: ")
+    quantidade1 = int(input("Insira a quantidade do produto: "))
+    dic1[produto1] = quantidade1
+print(dic1)
