a6011d262318d43a067b25984b4179f873344f01
Jonas Fernandes <diasjonas121@gmail.com>
2025-12-12 08:58:30 -0300

    v1

---
+++ ConjuntosAndDicionarios/AnalisandoVendasPorCategoria.py
@@ -0,0 +1,34 @@
+
+vendas = { 
+
+    "Eletrônicos": [ 
+
+        {"produto": "Smartphone", "quantidade": 5, "valor_unitario": 2000}, 
+
+        {"produto": "Tablet", "quantidade": 3, "valor_unitario": 1500} 
+
+    ], 
+
+    "Eletrodomésticos": [ 
+
+        {"produto": "Geladeira", "quantidade": 2, "valor_unitario": 3000}, 
+
+        {"produto": "Micro-ondas", "quantidade": 4, "valor_unitario": 800} 
+
+    ], 
+
+    "Livros": [ 
+
+        {"produto": "Livro A", "quantidade": 10, "valor_unitario": 50}, 
+
+        {"produto": "Livro B", "quantidade": 5, "valor_unitario": 100} 
+
+    ] 
+
+} 
+
+for categoria, lista_produtos in vendas.items():
+    segundo_produto = lista_produtos[1]
+    total = segundo_produto["quantidade"] * segundo_produto["valor_unitario"]
+    print(f"{categoria}: R${total}")
+
