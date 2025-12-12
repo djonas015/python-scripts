2c450acff5d5d59f567cf04443c134eb9e77f300
Jonas Fernandes <diasjonas121@gmail.com>
2025-12-11 21:55:39 -0300

    v1

--- ConjuntosAndDicionarios/Participantes_Maratona.py
+++ ConjuntosAndDicionarios/Participantes_Maratona.py
@@ -12,7 +12,7 @@ participantes = {
 listanomes = list(participantes.keys())
 listaidade = list(participantes.values())
 print(f"Nome dos participantes: {', '.join(listanomes)}")
-print(f"Idade dos participantes: ", *listaidade, sep=", ")
+print("Idade dos participantes: ", *listaidade, sep=", ")
 for i in range(4):
     print("Participantes e suas idades:"
           f" {listanomes[i]}, {listaidade[i]} anos")
