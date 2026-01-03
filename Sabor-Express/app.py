80dd846b085e38d5345c3a00978830178d1bd958
Jonas Fernandes <diasjonas121@gmail.com>
2026-01-03 12:43:51 -0300

    atualização projeto app

--- Sabor-Express/app.py
+++ Sabor-Express/app.py
@@ -1,5 +1,7 @@
 import os
 
+restaurantes = ["Pizza", "Sushi"]
+
 
 def exibir_nome_do_programa():
     print("""
@@ -16,24 +18,60 @@ def exibir_opcoes():
 
 
 def finalizar_app():
+    exibir_subtitulo("Finalizando o app")
+
+
+def voltar_ao_menu_principal():
+    input("\nDigite uma tecla para voltar ao menu principal: ")
+    main()
+
+
+def opcao_invalida():
+    print("Oção inválida!\n")
+    voltar_ao_menu_principal()
+
+
+def exibir_subtitulo(texto):
     os.system("cls")
-    print("Finalizando o app\n")
+    print(texto)
+    print()
+
+
+def cadastrar_novo_restaurante():
+    exibir_subtitulo("Cadastro de novos restaurantes")
+    nome_do_restaurante = input("Digite o nome do restaurante que deseja cadastrar: ")
+    restaurantes.append(nome_do_restaurante)
+    print(f"O Restaurante {nome_do_restaurante} foi cadastrado com sucesso!")
+    voltar_ao_menu_principal()
+
+
+def listar_restaurantes():
+    exibir_subtitulo("Listando os restaurantes")
+    for restaurante in restaurantes:
+        print(f".{restaurante}")
+    voltar_ao_menu_principal()
 
 
 def escolher_opcao():
-    opcao_escolhida = int(input("Escolha uma opção: "))
+    try:
+        opcao_escolhida = int(input("Escolha uma opção: "))
 
-    if opcao_escolhida == 1:
-        print("Cadastrar restaurante")
-    elif opcao_escolhida == 2:
-        print("Listar restaurante")
-    elif opcao_escolhida == 3:
-        print("Ativar restaurante")
-    else:
-        finalizar_app()
+        if opcao_escolhida == 1:
+            cadastrar_novo_restaurante()
+        elif opcao_escolhida == 2:
+            listar_restaurantes()
+        elif opcao_escolhida == 3:
+            print("Ativar restaurante")
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
 
 
 def main():
+    os.system("cls")
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
