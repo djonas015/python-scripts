3820521cf3189b01898154ce895f822ed566f310
Jonas Fernandes <diasjonas121@gmail.com>
2026-01-08 17:36:30 -0300

    update

--- Sabor-Express/app.py
+++ Sabor-Express/app.py
@@ -61,6 +61,22 @@ def listar_restaurantes():
     voltar_ao_menu_principal()
 
 
+def alternar_estado_restaurante():
+    exibir_subtitulo("Alternando estado do restaurante")
+    nome_restaurante = input("Insira o nome do restaurante que deseja alternar o estado: ")
+    restaurante_encontrado = False
+    for restaurante in restaurantes:
+        if nome_restaurante == restaurante["nome"]:
+            restaurante_encontrado = True
+            restaurante["ativo"] = not restaurante["ativo"]
+            mensagem = f"O restaurante {nome_restaurante} foi ativado com sucesso" if restaurante["ativo"] else f"O restaurante {nome_restaurante} foi desativado com sucesso"
+            print(mensagem)
+            
+    if not restaurante_encontrado:
+        print("O restaurante não foi encontrado")
+    voltar_ao_menu_principal()
+
+
 def escolher_opcao():
     try:
         opcao_escolhida = int(input("Escolha uma opção: "))
@@ -70,7 +86,7 @@ def escolher_opcao():
         elif opcao_escolhida == 2:
             listar_restaurantes()
         elif opcao_escolhida == 3:
-            print("Ativar restaurante")
+            alternar_estado_restaurante()
         elif opcao_escolhida == 4:
             finalizar_app()
         else:
