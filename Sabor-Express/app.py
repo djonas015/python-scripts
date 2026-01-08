3db57d1e3e7f1478896c441637b1783a36f5460a
Jonas Fernandes <diasjonas121@gmail.com>
2026-01-08 09:02:53 -0300

    update

--- Sabor-Express/app.py
+++ Sabor-Express/app.py
@@ -1,6 +1,8 @@
 import os
 
-restaurantes = ["Pizza", "Sushi"]
+restaurantes = [{"nome": "Pizza", "categoria": "Italiana", "ativo": False},
+                {"nome": "Sushi", "categoria": "Japonesa", "ativo": True},
+                {"nome": "Cantina", "categoria": "Brasileira", "ativo": False}]
 
 
 def exibir_nome_do_programa():
@@ -40,7 +42,11 @@ def exibir_subtitulo(texto):
 def cadastrar_novo_restaurante():
     exibir_subtitulo("Cadastro de novos restaurantes")
     nome_do_restaurante = input("Digite o nome do restaurante que deseja cadastrar: ")
-    restaurantes.append(nome_do_restaurante)
+    categoria = input(f"Insira o nome da categoria do restaurante {nome_do_restaurante}: ")
+    dados_do_restaurante = {"nome": nome_do_restaurante,
+                            "categoria": categoria,
+                            "ativo": False}
+    restaurantes.append(dados_do_restaurante)
     print(f"O Restaurante {nome_do_restaurante} foi cadastrado com sucesso!")
     voltar_ao_menu_principal()
 
@@ -48,7 +54,10 @@ def cadastrar_novo_restaurante():
 def listar_restaurantes():
     exibir_subtitulo("Listando os restaurantes")
     for restaurante in restaurantes:
-        print(f".{restaurante}")
+        nome_restaurante = restaurante["nome"]
+        categoria_restaurante = restaurante["categoria"]
+        ativo = restaurante["ativo"]
+        print(f".{nome_restaurante} | {categoria_restaurante} | {ativo}")
     voltar_ao_menu_principal()
 
 
